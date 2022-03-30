7b8c3e4a2fccd9a989384ce9b4fb98ff4ba75cfd
Monika Swoboda <mon.swoboda@gmail.com>
2022-03-30 13:24:42 +0200

    Get href attribute

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function titleClickHandler(event){
-  const clickedElement = this;
-  console.log('Link was clicked!');
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('Link was clicked!');
  // console.dirxml(event);
 
   /* [DONE] remove class 'active' from all article links  */
@@ -13,10 +14,10 @@ function titleClickHandler(event){
   activeLink.classList.remove('active');
 }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
-  console.log('clickedElement (with plus): ' + clickedElement);
+  // console.log('clickedElement (with plus): ' + clickedElement);
   clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
@@ -27,7 +28,9 @@ function titleClickHandler(event){
   activeArticle.classList.remove('active');
 }
 
-  /* get 'href' attribute from the clicked link */
+  /* [IN PROGRESS] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute("href");
+    console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
