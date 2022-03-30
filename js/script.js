2b87b8ce354346c7be2c8b7b8d32c1a9f15c458d
Monika Swoboda <mon.swoboda@gmail.com>
2022-03-30 12:16:51 +0200

    Add class active to clicked link

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.dirxml(event);
+ // console.dirxml(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +13,13 @@ function titleClickHandler(event){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+  console.log('clickedElement (with plus): ' + clickedElement);
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
 
