06f38eac0bd495d3c830f6d2312fb83c8b492f3b
Monika Swoboda <mon.swoboda@gmail.com>
2022-04-01 11:48:18 +0200

    Install eslint and modifyjs file

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 'use strict';
 
 function titleClickHandler(event){
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!');
- // console.dirxml(event);
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
+  // console.dirxml(event);
 
   /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+    activeLink.classList.remove('active');
+  }
   /* [DONE] add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
@@ -24,22 +24,22 @@ function titleClickHandler(event){
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+    activeArticle.classList.remove('active');
+  }
 
   /* [DONE] get 'href' attribute from the clicked link */
 
-    const articleSelector = clickedElement.getAttribute("href");
-    console.log(articleSelector);
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-    const targetArticle = document.querySelector(articleSelector);
-    console.log(targetArticle);
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
   /* [DONE] add class 'active' to the correct article */
 
-    targetArticle.classList.add('active');
+  targetArticle.classList.add('active');
 }
 
 const optArticleSelector = '.post',
@@ -59,7 +59,7 @@ function generateTitleLinks(){
   for(let article of articles){
   
     /* [DONE] get the article id */
-    const articleId = article.getAttribute("id");
+    const articleId = article.getAttribute('id');
     console.log(articleId);
     
     /* [DONE] find the title element */
@@ -72,27 +72,28 @@ function generateTitleLinks(){
     console.log(linkHTML);
     
     /* insert link into titleList */
-    const titles = document.querySelector('.titles')
-    titles.insertAdjacentHTML("afterbegin", linkHTML);
+    const titles = document.querySelector('.titles');
+    titles.insertAdjacentHTML('afterbegin', linkHTML);
 
     /* Pytania do mentora
         1. dlaczego nie dziala ze stala optTitleListSelector??
         2. dlaczego nie dziala titleList.innerHTML = titleList.innerHTML + linkHTML; 
-        3. czemu nie dziala html i titleList */
+        3. czemu nie dziala html i titleList 
+        4. Wedlug podsumowania strona powinna wygladac tak samo - nie wyglada, artykuly sa ulozone malejaco a nie rosnaco*/
     
-   // html = html + linkHTML;    
+    // html = html + linkHTML;    
   }
     
-   // console.log(html);
+  // console.log(html);
 
-   // titleList.innerHTML = html;
+  // titleList.innerHTML = html;
 
-    const links = document.querySelectorAll('.titles a');
-    console.log(links);
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
 
-    for(let link of links){
+  for(let link of links){
     link.addEventListener('click', titleClickHandler);
-}
+  }
 
 }
 
